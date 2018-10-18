de9beac57c85ff40d234097af28a7a45f0c2c0d8
Andrea Belen Figueroa Retamal <abfiguer@alumnos.inf.utfsm.cl>
2018-10-18 17:19:21 -0300

    Rails application and general layout of coding view

---
+++ test/controllers/coding_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class CodingControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get coding_index_url
+    assert_response :success
+  end
+
+end
